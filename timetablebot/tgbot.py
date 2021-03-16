f8705178a0aeb6ce17aa27754d5239d57751314c
Azim Khakulov <azim@khakulov.com>
2021-03-16 23:30:01 +0500

    bot

--- timetablebot/tgbot.py
+++ timetablebot/tgbot.py
@@ -11,186 +11,201 @@ from telegram.ext import (
     ConversationHandler,
     CallbackContext,
 )
-# Enable logging
-TOKEN = os.environ.get("TELEGRAM_BOT_TOKEN")
-logging.basicConfig(
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
-)
 
-USER_ID = 1  # default 1 but when we want to update a user we just put its id
-USER_ENDPOINT = "http://127.0.0.1:8000/api/booking/user/"
-ACCESS_TOKEN_ENDPOINT = "http://127.0.0.1:8000/api/booking/token/"
-QUESTIONS_ENDPOINT = "http://127.0.0.1:8000/api/assessment/question/"
-ANSWER_ENDPOINT = "http://127.0.0.1:8000/api/assessment/answer/"
-UPDATE_USER_INFO_ENDPOINT = f"http://127.0.0.1:8000/api/booking/user/{USER_ID}"
+from timetablebot.timetable import (
+    get_userinfo,
+    create_telegram_user,
+    update_telegram_user,
+    get_subjects,
+    get_teachers,
+    get_questions,
+    create_answer,
+)
+from timetablebot.utils import build_menu
+
+
+# Enable logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 logger = logging.getLogger(__name__)
 
+TOKEN = os.environ.get("TELEGRAM_BOT_TOKEN")
+TIMETABLE_URL = os.environ.get("TIMETABLE_URL")
+
+QUESTIONS_ENDPOINT = f"{TIMETABLE_URL}/api/question/"
+ANSWER_ENDPOINT = f"{TIMETABLE_URL}/api/answer/"
+
 STUDENT_ID, DATE_OF_BIRTH, EDUCATION_YEAR, FACULTY, SUBJECTS, TEACHER, QUESTION, ANSWER = range(8)
-reg_data = {}
-def start(update: Update, context: CallbackContext) -> int:
-    telegram_id = update.message.from_user.id
-    print(telegram_id)
-    # print(telegram_id)
-    data = {
-        "telegram_id": telegram_id
-    }
-    print("I am here")
-    response = requests.get(url=USER_ENDPOINT, data=data)
-    time.sleep(2)
-    print(response.json())
 
 
-    if "telegram_id" in response.json():
-        reply_keyboard = [['fizika', 'matematika', 'informatika']]
-        update.message.reply_text(
-            "Thank you so much"
-            "Please choose which subject do you want to give your comments",
-            reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True)
-        )
-        return SUBJECTS
-    else:
-        update.message.reply_text(
-        "Hello, this bot serves you to assess teacher in TTPU."
-        "Please enter your student id in numbers like: (12259)"
+def get_student_id(update: Update):
+    update.message.reply_text(
+        "Please enter your student id in numbers like: (12259) "
         "If you do not want to assess teachers simply click here /cancel."
-        )
-        return STUDENT_ID
+    )
+    return STUDENT_ID
+
+
+def get_date_of_birth(update: Update):
+    update.message.reply_text(
+        "I see. Great, please enter your date of birth altogether (DDMMYYYY)"
+    )
+    return DATE_OF_BIRTH
+
+
+def get_education_year(update: Update):
+    reply_keyboard = [['1', '2', '3', '4']]
+    update.message.reply_text(
+        "Please tell me about your education year "
+        "for example (1, 2, 3, 4)",
+        reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True),
+    )
+    return EDUCATION_YEAR
+
+
+def get_faculty(update: Update):
+    update.message.reply_text("Now you need to tell me your faculty")
+    return FACULTY
+
+
+def get_subject(update: Update, context: CallbackContext):
+    telegram_id = update.message.from_user.id
+    logger.info(f"{telegram_id}: Get subjects from server.")
+    subjects = get_subjects(telegram_id)
+    context.user_data['subjects'] = subjects
+
+    reply_keyboard = [subject['name'] for subject in subjects]
+    update.message.reply_text(
+        "Please choose which subject do you want to give your comments",
+        reply_markup=ReplyKeyboardMarkup(build_menu(reply_keyboard, 3), one_time_keyboard=True)
+    )
+
+    return SUBJECTS
+
+
+def get_teacher(update: Update, context: CallbackContext):
+    telegram_id = update.message.from_user.id
+    logger.info(f"{telegram_id}: Get teachers from server.")
+    teachers = get_teachers(telegram_id)
+    context.user_data['teachers'] = teachers
+
+    reply_keyboard = [teacher['short'] for teacher in teachers]
+
+    update.message.reply_text(
+        "You need to choose which teacher you wanna comment ",
+        reply_markup=ReplyKeyboardMarkup(build_menu(reply_keyboard, 5), one_time_keyboard=True)
+    )
+    return TEACHER
+
+
+def start(update: Update, context: CallbackContext) -> int:
+    update.message.reply_text("Hello, this bot serves you to assess teacher in TTPU.")
+
+    telegram_id = update.message.from_user.id
+    logger.info(f"{telegram_id}: New user started the bot.")
+    userinfo = get_userinfo(telegram_id)
+
+    if 'id' in userinfo and userinfo['faculty'] is not None and userinfo['education_year'] is not None:
+        logger.info(f"{telegram_id}: User has already registered.")
+        return get_subject(update, context)
+    elif 'id' in userinfo:
+        logger.info(f"{telegram_id}: User has already registered, but has not faculty or education_year.")
+        return get_education_year(update)
+    else:
+        logger.info(f"{telegram_id}: User is not yet registered.")
+        return get_student_id(update)
+
 
 def cancel(update: Update, context: CallbackContext) -> int:
     update.message.reply_text(
         'Ok, If you want to add more value to TTPU we are here'
         "Just click here /start "
     )
-
     return ConversationHandler.END
 
+
 def student_id(update: Update, context: CallbackContext) -> int:
-    user = update.message.from_user
-    student_id = update.message.text
-    context.user_data['username'] = student_id
-    context.user_data['telegram_id'] = user.id
-    update.message.reply_text(
-        "I see.Great, please enter your date of birth altogether(DDMMYYYY)"
-    )
-    return DATE_OF_BIRTH
+    context.user_data['username'] = update.message.text.strip()
+    return get_date_of_birth(update)
+
 
 def date_of_birth(update: Update, context: CallbackContext) -> int:
-    date_of_birth = update.message.text
-    context.user_data['password'] = str(date_of_birth)
-    context.user_data['raw_password'] = date_of_birth
-    print(context.user_data['raw_password'])
-
-    reply_keyboard = [['1', '2', '3', '4']]
-    update.message.reply_text(
-        "Thank you so much for your patience and effort."
-        "Please tell me about your education year"
-        "for example (1, 2, 3, 4)",
-        reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True),
+    update.message.reply_text("Thank you so much.")
+    
+    telegram_id = update.message.from_user.id
+    logger.info(f"{telegram_id}: Check if user exist in Database.")
+    userinfo = create_telegram_user(
+        telegram_id=update.message.from_user.id,
+        username=context.user_data['username'],
+        email=update.message.text.strip()
     )
-    return EDUCATION_YEAR
+    if 'id' in userinfo:
+        logger.info(f"{telegram_id}: User exist in Database.")
+        return get_education_year(update)
+    else:
+        logger.info(f"{telegram_id}: User is not exist in Database.")
+        update.message.reply_text("Unfortunately, we could not find you in our system.")
+        return get_student_id(update)
+
 
 def education_year(update: Update, context: CallbackContext) -> int:
-    education_year = update.message.text
-    context.user_data['education_year'] = int(education_year)
-    update.message.reply_text(
-        "Now you need to tell me your faculty"
-    )
-    return FACULTY
+    update.message.reply_text("Thank you so much for your patience and effort.")
+    context.user_data['education_year'] = int(update.message.text)
+    return get_faculty(update)
+
+
 def faculty(update: Update, context: CallbackContext) -> int:
-    faculty = update.message.text
-    context.user_data['faculty'] = faculty
-    # creating a new user
-    data = {
-        "username": context.user_data['username'],
-        "password": context.user_data['password'],
-        "faculty": context.user_data['faculty'],
-        "education_year": context.user_data['education_year'],
-        "telegram_id": context.user_data['telegram_id'],
-        "raw_password": context.user_data['raw_password']
-    }
-    requests.post(url=USER_ENDPOINT, data=data)
-    time.sleep(2)
+    update.message.reply_text("Cool. One more thing.")
 
-    reply_keyboard = [['fizika', 'matematika', 'informatika']]
-    update.message.reply_text(
-        "Thank you so much"
-        "Please choose which subject do you want to give your comments",
-        reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True)
+    telegram_id = update.message.from_user.id
+    logger.info(f"{telegram_id}: Update users education_year and faculty.")
+    update_telegram_user(
+        telegram_id=telegram_id,
+        education_year=context.user_data['education_year'],
+        faculty=update.message.text
     )
 
-    return SUBJECTS
+    return get_subject(update, context)
+
 
 def subjects(update: Update, context: CallbackContext) -> int:
-    # get authorization token if user starts from subjects
-    telegram_id = update.message.from_user.id
-    body = {
-        "telegram_id": telegram_id
-    }
-    response = requests.get(url=USER_ENDPOINT, data=body)
-    time.sleep(2)
-    user = response.json()
-    print(user)
-    # get access token
+    update.message.reply_text("Thank you so much.")
+    context.user_data['subject'] = update.message.text
+    return get_teacher(update, context)
 
-    get_token_body = {
-        "username": user['username'],
-        "password": user['raw_password']
-    }
-    response = requests.post(url=ACCESS_TOKEN_ENDPOINT, data=get_token_body)
-    time.sleep(2)
-    access_token = response.json()['token']
-    print(access_token)
-    context.user_data['token'] = access_token
-
-    subject = update.message.text
-    context.user_data['subject'] = subject
-    reply_keyboard = [['mirahmad', 'anvar', 'botir']]
-    update.message.reply_text(
-        "You need to choose which teacher you wanna comment ",
-        reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True)
-    )
-    return TEACHER
 
 def teacher(update: Update, context: CallbackContext) -> int:
-    teacher = update.message.text
-    context.user_data['teacher'] = teacher
-    access_token = context.user_data['token']
+    context.user_data['teacher'] = update.message.text
+
     # get a question from database
-    response = requests.get(url=QUESTIONS_ENDPOINT,
-                            headers={"Authorization": f"Token {access_token}"}
-                            )
-    # letting server some time
-    time.sleep(2)
-    question1 = response.json()[0]['question_text']  # just default question
-    context.user_data['question'] = 1  # just default id
-    update.message.reply_text(
-        "Please, answer to this question"
-        f"{question1}"
-    )
+    questions = get_questions(update.message.from_user.id)
+
+    if len(questions) == 0:
+        update.message.reply_text("Sorry, but there are no querstions.")
+        return ConversationHandler.END
+
+    question1 = questions[0]['question_text']  # just default question
+    context.user_data['question'] = questions[0]['id'] # just default id
+    update.message.reply_text(question1)
 
     return ANSWER
 
 
 def answer(update: Update, context: CallbackContext) -> int:
-    answer = update.message.text
-    context.user_data['answer'] = answer
+    telegram_id = update.message.from_user.id
+    subject = next(subject['id'] for subject in context.user_data['subjects'] if subject['name'] == context.user_data['subject'])
+    teacher = next(teacher['id'] for teacher in context.user_data['teachers'] if teacher['short'] == context.user_data['teacher'])
 
-    data = {
-        "question": context.user_data['question'],
-        "answer": context.user_data['answer'],
-        "subject": context.user_data['subject'],
-        "teacher": context.user_data['teacher']
-    }
-    access_token = context.user_data['token']
-    header = {
-        "Authorization": f"Token {access_token}"
-    }
     # post answer
-    requests.post(url=ANSWER_ENDPOINT, data=data, headers=header)
-    time.sleep(2)
+    create_answer(
+        telegram_id=telegram_id,
+        subject=subject,
+        teacher=teacher,
+        question=context.user_data['question'],
+        answer=update.message.text,
+    )
     update.message.reply_text(
-        "Thank you for your time and effort"
-        "You have added value to our growth"
+        "Thank you for your time and effort. "
+        "You have added value to our growth. "
         "Do you want to give comment click here /start "
     )
     return ConversationHandler.END
---
+++ timetablebot/timetable.py
@@ -0,0 +1,90 @@
+import os
+import requests
+
+TIMETABLE_URL = os.environ.get("TIMETABLE_URL")
+TIMETABLE_TOKEN = os.environ.get("TIMETABLE_TOKEN")
+
+TELEGRAM_USER_ENDPOINT = f"{TIMETABLE_URL}/api/telegramuser/"
+TELEGRAM_BOT_ENDPOINT = f"{TIMETABLE_URL}/api/telegrambot/"
+SUBJECT_ENDPOINT = f"{TIMETABLE_URL}/api/subject/"
+TEACHER_ENDPOINT = f"{TIMETABLE_URL}/api/teacher/"
+
+QUESTION_ENDPOINT = f"{TIMETABLE_URL}/api/question/"
+ANSWER_ENDPOINT = f"{TIMETABLE_URL}/api/answer/"
+
+
+def get_userinfo(telegram_id: str):
+    headers = {
+        'Authorization': f'Bot {TIMETABLE_TOKEN}',
+        'Telegram-ID': f'{telegram_id}'
+    }
+    response = requests.get(url=TELEGRAM_USER_ENDPOINT, headers=headers)
+    return response.json()
+
+
+def create_telegram_user(telegram_id: str, username: str, email: str):
+    headers = {
+        'Authorization': f'Bot {TIMETABLE_TOKEN}',
+    }
+    data = {
+        'telegram_id': f'{telegram_id}',
+        'username': username,
+        'email': email,
+    }
+    response = requests.post(url=TELEGRAM_BOT_ENDPOINT, headers=headers, data=data)
+    return response.json()
+
+
+def update_telegram_user(telegram_id: str, education_year: int, faculty: str):
+    headers = {
+        'Authorization': f'Bot {TIMETABLE_TOKEN}',
+        'Telegram-ID': f'{telegram_id}'
+    }
+    data = {
+        'education_year': username,
+        'faculty': email,
+    }
+    response = requests.post(url=TELEGRAM_USER_ENDPOINT, headers=headers, data=data)
+    return response.json()
+
+
+def get_subjects(telegram_id: str):
+    headers = {
+        'Authorization': f'Bot {TIMETABLE_TOKEN}',
+        'Telegram-ID': f'{telegram_id}'
+    }
+    response = requests.get(url=SUBJECT_ENDPOINT, headers=headers)
+    return response.json()
+
+
+def get_teachers(telegram_id: str):
+    headers = {
+        'Authorization': f'Bot {TIMETABLE_TOKEN}',
+        'Telegram-ID': f'{telegram_id}'
+    }
+    response = requests.get(url=TEACHER_ENDPOINT, headers=headers)
+    return response.json()
+
+
+def get_questions(telegram_id: str):
+    headers = {
+        'Authorization': f'Bot {TIMETABLE_TOKEN}',
+        'Telegram-ID': f'{telegram_id}'
+    }
+    response = requests.get(url=QUESTION_ENDPOINT, headers=headers)
+    return response.json()
+
+
+def create_answer(telegram_id: str, subject: int, teacher: int, question: int, answer: str):
+    headers = {
+        'Authorization': f'Bot {TIMETABLE_TOKEN}',
+        'Telegram-ID': f'{telegram_id}'
+    }
+    data = {
+        "subject": subject,
+        "teacher": teacher,
+        "question": question,
+        "answer": answer,
+    }
+    response = requests.post(url=ANSWER_ENDPOINT, headers=headers, data=data)
+    return response.json()
